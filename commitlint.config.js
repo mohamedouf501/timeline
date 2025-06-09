15395bbf4df97873fd9acca16b6d3f46077adda3
mohamedouf501 <mohamedouf501@gmail.com>
2025-06-09 23:54:06 +0300

    feat(commitlint): enhance commitlint configuration with detailed rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,31 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'type-empty': [2, 'never'],
+    'scope-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100],
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+  },
+};
