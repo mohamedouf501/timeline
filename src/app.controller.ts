2d34f76af4c27da1fef836c373a3d30c474d14ee
mohamedouf501 <mohamedouf501@gmail.com>
2025-06-10 00:42:52 +0300

    feat: add endpoint to retrieve changelog from CHANGELOG.md

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import * as fs from 'fs';
+import * as path from 'path';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('changelog')
+  getChangelog(): string {
+    const changelogPath = path.join(process.cwd(), 'CHANGELOG.md');
+
+    if (fs.existsSync(changelogPath)) {
+      return fs.readFileSync(changelogPath, 'utf-8');
+    } else {
+      return 'CHANGELOG.md not found.';
+    }
   }
 }
