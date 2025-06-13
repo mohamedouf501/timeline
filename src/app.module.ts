f95f3e334459bcf5df116db4b12f2f6d1c0d56e5
mohamedouf501 <mohamedouf501@gmail.com>
2025-06-13 20:02:16 +0300

    feat: add UsersModule, PostsModule, and FriendshipsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './infrastructure/databases/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { FriendshipsModule } from './modules/friendships/friendships.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    UsersModule,
+    PostsModule,
+    FriendshipsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
