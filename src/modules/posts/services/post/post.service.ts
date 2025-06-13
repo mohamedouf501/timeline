13c511ca4d1fb61d7fd6cf267de3cc9ce2ed5e4d
mohamedouf501 <mohamedouf501@gmail.com>
2025-06-13 20:01:59 +0300

    feat: add PostService with create and update methods for managing posts

---
+++ src/modules/posts/services/post/post.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreatePostDto } from '../../dtos/create-post.dto';
+import { UpdatePostDto } from '../../dtos/update-post.dto';
+import { IPostRepository } from '../../repositories/post.repository.interface';
+
+@Injectable()
+export class PostService {
+  constructor(
+    @Inject('IPostRepository')
+    private readonly postRepo: IPostRepository,
+  ) {}
+
+  async create(dto: CreatePostDto) {
+    return this.postRepo.createPost(dto);
+  }
+
+  async update(dto: UpdatePostDto) {
+    await this.postRepo.updatePost(dto);
+  }
+}
